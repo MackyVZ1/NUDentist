b7a6009908d99d3c0a7eb7d2a0f2406ff8e995e5
MackyVZ1 <152200588+MackyVZ1@users.noreply.github.com>
2024-07-31 09:49:15 +0700

    delete all files in folder 'website'

--- website/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 8000
-app.get("/", (req, res) => {
-    res.json({message : 'Hello World!'})
-})
-
-app.listen(port,console.log("server is running on the port"));
